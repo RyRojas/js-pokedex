02911a420cec738d78fe8e7bcb9f700ecb825f4a
RyRojas <30281811+RyRojas@users.noreply.github.com>
2021-02-17 12:00:21 -0800

    Corrected string to use single quotes
    How embarrassing.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,4 @@
 alert('Hello world');
 
-let favoriteFood = "I really love all food, I will not pick one.";
+let favoriteFood = 'I really love all food, I will not pick one.';
 document.write(favoriteFood);
