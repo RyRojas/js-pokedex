028682826a1e5bf463e171757a169e94eafcbfdb
RyRojas <30281811+RyRojas@users.noreply.github.com>
2021-02-17 16:40:10 -0800

    Added for loop and basic styling to html per spec (Ex 1.3)

--- js/scripts.js
+++ js/scripts.js
@@ -20,3 +20,17 @@ pokemonList = [
         type: ['electric'],
     },
 ];
+
+//Iterate through each Pokemon in our list
+for (let i = 0; i < pokemonList.length; i++) {
+
+    //Initialize variable to build complete message, scope remians within loop block
+    let pokemonJudgement = `${pokemonList[i].name} (Height: ${pokemonList[i].height}m)`;
+    
+    //Find the less short guys
+    if (pokemonList[i].height > 0.5) {
+        pokemonJudgement = pokemonJudgement + ' - WOW! That\'s big. Well...it\'s not <b>too</b> short';
+    }
+
+    document.write(`${pokemonJudgement} <br>`);
+}
