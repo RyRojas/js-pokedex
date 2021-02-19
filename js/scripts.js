2b18f2985455bd75a516544a2f7af2a326447eb2
RyRojas <30281811+RyRojas@users.noreply.github.com>
2021-02-18 17:08:24 -0800

    Converted array to IIFE and built various functions into the repository per spec (Ex 1.5.2)

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,69 @@
-let pokemonList = [
-    {
-        name: 'Bulbasaur',
-        height: 0.7,
-        type: ['grass', 'poison'],
-    }, 
-    {
-        name: 'Charmander',
-        height: 0.6,
-        type: ['fire'],
-    }, 
-    {
-        name: 'Squirtle',
-        height: 0.5,
-        type: ['water'],
-    }, 
-    {
-        name: 'Pikachu',
-        height: 0.4,
-        type: ['electric'],
-    },
-];
+let pokemonRepository = (function() {
+    let pokemonList = [
+        {
+            name: 'Bulbasaur',
+            height: 0.7,
+            type: ['grass', 'poison']
+        },
+        {
+            name: 'Charmander',
+            height: 0.6,
+            type: ['fire']
+        },
+        {
+            name: 'Squirtle',
+            height: 0.5,
+            type: ['water']
+        },
+        {
+            name: 'Pikachu',
+            height: 0.4,
+            type: ['electric']
+        }
+    ];
+
+    function getAll() {
+        return pokemonList;
+    }
+
+    function add(pokemon) {
+        if (typeof(pokemon) === 'object') {
+            //Store the object keys for first object in array as well as the requested entry
+            let pokeKeys = Object.keys(pokemonList[0])
+            let entryKeys = Object.keys(pokemon)
+
+            //Dummy variable to store results of conditional statement
+            let match = null;
+            //Iterate through both key arrays in order to compare them (switched back to for loop to match by index)
+            for (let i = 0; i < pokeKeys.length; i++)
+                if (pokeKeys[i] !== entryKeys[i]) {
+                    match = false;
+                    return console.log('Object properties do not match');
+                } else {
+                    match = true;
+                }
+            if (match) {
+                return pokemonList.push(pokemon);
+            }
+        } else {
+            return console.log('Pokemon entered is not an object')
+        }
+    }
+
+    function find(pokemonSearch) {
+       return pokemonList.filter(pokemon => pokemon.name === pokemonSearch);
+    }
+
+    return {
+        getAll: getAll,
+        add: add,
+        find: find
+    }
+})();
 
 //Iterate through each Pokemon in our list (now in forEach cinemascope)
-pokemonList.forEach(function(pokemon) {
-    //Initialize variable to build complete message, scope remians within loop block
+pokemonRepository.getAll().forEach(function(pokemon) {
+    //Initialize variable to build complete message, scope remains within block
     let pokemonJudgement = `${pokemon.name} (Height: ${pokemon.height}m)`;
     
     //Find the less short guys
@@ -33,3 +73,17 @@ pokemonList.forEach(function(pokemon) {
 
     document.write(`${pokemonJudgement} <br>`);
 });
+
+//BLOCK TO TEST ADD FUNCTION
+//---------------------------
+// let x = {
+//     name: 'Pokemon Man',
+//     height: 1.2,
+//     type: ['Normal']
+// };
+//
+//pokemonRepository.add(x)
+
+//BLOCK TO TEST SEARCH FUNCTION
+//-----------------------------
+//console.log(pokemonRepository.find('Squirtle'));
